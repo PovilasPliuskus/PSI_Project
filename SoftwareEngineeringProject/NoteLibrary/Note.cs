9bfbd14db0c3c1aa5ed20f4f97bb547f79855570
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2023-09-15 18:17:56 +0300

    Add Note.cs with some initial fields and properties

---
+++ SoftwareEngineeringProject/NoteLibrary/Note.cs
@@ -0,0 +1,54 @@
+﻿namespace SoftwareEngineeringProject.NoteLibrary
+{
+    public class Note
+    {
+        private String id;
+        private String name;
+        private String value;
+        private int rows;
+        private int columns;
+
+        public String ID
+        {
+            get { return id; }
+        }
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public String Value
+        {
+            get { return value; }
+            set { this.value = value.ToString(); }
+        }
+
+        public int Rows
+        {
+            get { return rows; }
+            set { rows = value; }
+        }
+
+        public int Columns
+        {
+            get { return columns; }
+            set { columns = value; }
+        }
+
+        public Note(string id, string name, string value, int rows, int columns)
+        {
+            this.id = id;
+            this.name = name;
+            this.value = value;
+            this.rows = rows;
+            this.columns = columns;
+        }
+
+        public string Render()
+        {
+            return $"<textarea>{Value}</textarea>";
+        }
+    }
+}
