c1c8842b44c1675f5fb11f19e053ef7ed717745a
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2023-11-18 16:28:41 +0200

    Clean NoteDBContext file

--- SoftwareEngineeringProject/Models/NoteDBContext.cs
+++ SoftwareEngineeringProject/Models/NoteDBContext.cs
@@ -6,17 +6,9 @@ namespace SoftwareEngineeringProject.Models
     {
         public DbSet<Note> Notes { get; set;}
 
-        // NoteDBContext
         public NoteDBContext(DbContextOptions options) : base(options)
         {
             
         }
-/*        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<Note>()
-                .HasKey(n => n.Id);
-        }*/
     }
 }
